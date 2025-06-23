8893a205521097d1e194653d8f562f2c0f8c9151
Astitva Shukla <168917777+astitvashukla05@users.noreply.github.com>
2025-06-23 12:27:05 +0530

    Delete src directory

--- src/Components/TabButtons.jsx
+++
@@ -1,9 +0,0 @@
-function TabButtons(props){
-    // function handleClick(){
-    //     console.log("Hello")
-    // }
-    return(
-        <li><button className={props.isactive ? "active" :null} onClick={props.onSelect}> {props.children} </button></li>
-    )
-}
-export default TabButtons
